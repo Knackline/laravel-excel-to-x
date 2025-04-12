081ab76683bb935bc8d0b7a50a1124ef6a690b71
RAJKUMAR <rajkumarsamra@gmail.com>
2025-04-12 15:56:04 +0530

    test: add feature tests for Excel streaming functionality

---
+++ tests/Feature/ExcelToStreamTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Knackline\ExcelTo\Tests\Feature;
+
+use Knackline\ExcelTo\ExcelTo;
+use PHPUnit\Framework\TestCase;
+
+class ExcelToStreamTest extends TestCase
+{
+    // ... existing test code ...
+}
